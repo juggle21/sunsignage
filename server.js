fc0349a1bca9ad70b301320ab3b61751343c19e0
sunsignage <juggle21@naver.com>
2016-04-20 11:32:17 +0000

    server.js eidt

--- server.js
+++ server.js
@@ -35,15 +35,15 @@
 
 	mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
 
-    app.use('/js', express.static(__dirname + '/js'));
-   	app.use('/bower_components', express.static(__dirname + '/bower_components'));
+    //app.use('/js', express.static(__dirname + '/js'));
+   	//app.use('/bower_components', express.static(__dirname + '/bower_components'));
 	app.use(morgan('dev')); 										// log every request to the console
 	app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 	app.use(bodyParser.json()); 									// parse application/json
 	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 	app.use(methodOverride());
 
-	// define model =================
+	/* define model =================
 	var Todo = mongoose.model('Todo', {
 		title : String,
 		completed: Boolean
@@ -124,5 +124,5 @@
 	});
 
 	// listen (start app with node server.js) ======================================
-	app.listen(80, argv.fe_ip);
-	console.log("App listening on port 80");
+	app.listen(8080, argv.fe_ip);
+	console.log("App listening on port 8080");
